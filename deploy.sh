e7c55c027402a953d1aac8df4650e75b516fdc89
brycepc <brycepc@hotmail.com>
2025-01-24 16:58:08 +1300

    Adding vpc subnets

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-vpcCidr="10.1.0.0/16"
 environment="prod"
 stackName="tbs-infra-vpc"
 region="ap-southeast-2"
@@ -8,7 +7,6 @@ region="ap-southeast-2"
 aws cloudformation deploy --template-file ./cloudformation/template.yaml --stack-name $stackName \
   --parameter-overrides \
     Environment=${environment} \
-    VPCCidr=$vpcCidr \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --region $region --no-fail-on-empty-changeset \
   --tags stackname=$stackName environment=$environment \
   --profile thebetterstore
