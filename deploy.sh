896e47a4095104101e53eeb731645098f51bc15e
brycepc <brycepc@hotmail.com>
2025-01-24 17:43:50 +1300

    Fixing stackName

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+appName="tbs-infra-vpc"
 environment="prod"
-stackName="tbs-infra-vpc"
+stackName="$appName-$environment"
 region="ap-southeast-2"
 
 aws cloudformation deploy --template-file ./cloudformation/template.yaml --stack-name $stackName \
   --parameter-overrides \
     Environment=${environment} \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --region $region --no-fail-on-empty-changeset \
-  --tags stackname=$stackName environment=$environment \
+  --tags appName=$appName stackname=$stackName environment=$environment \
   --profile thebetterstore
